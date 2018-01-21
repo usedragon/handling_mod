01ee308ab3cf37bd3037191a140cf2201179642f
usedragon <usedragon@hotmail.com>
2018-01-21 08:32:47 -0600

    start
    handling resource for FiveM and might use it on Revolution RP
    (forums.rmog.us)

---
+++ handling_mod/handling.lua
@@ -0,0 +1,41 @@
+function drawNotification(text)
+	SetNotificationTextEntry("STRING")
+	AddTextComponentString(text)
+	DrawNotification(false, false)
+end
+
+function DrawMissionText2(m_text, showtime)
+    ClearPrints()
+	SetTextEntry_2("STRING")
+	AddTextComponentString(m_text)
+	DrawSubtitleTimed(showtime, 1)
+end
+
+function drawTxt(text,font,centre,x,y,scale,r,g,b,a)
+	SetTextFont(font)
+	SetTextProportional(0)
+	SetTextScale(scale, scale)
+	SetTextColour(r, g, b, a)
+	SetTextDropShadow(0, 0, 0, 0,255)
+	SetTextEdge(1, 0, 0, 0, 255)
+	SetTextDropShadow()
+	SetTextOutline()
+	SetTextCentre(centre)
+	SetTextEntry("STRING")
+	AddTextComponentString(text)
+	DrawText(x , y)
+end
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+		DrawMissionText2("Handling Version: ~g~0.0.1~s~")
+	end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+		drawNotification("Motorcycle")
+	end
+end)
