efdc18a8b12d02caf64ad2f540b9698687a168fe
usedragon <usedragon@hotmail.com>
2018-01-21 19:21:47 -0600

    0.0.3.1-hotfix
    reverting back fInitialDragCoeff

--- handling_mod/handling.lua
+++ handling_mod/handling.lua
@@ -29,7 +29,7 @@ end
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-		DrawMissionText2("Handling Version: ~g~0.0.2~s~")
+		DrawMissionText2("Handling Version: ~g~0.0.3.1-hotfix~s~")
 	end
 end)
 
