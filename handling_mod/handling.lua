c2c396cfd5663054dfc68392724bd1b5bcbd4f1d
usedragon <usedragon@hotmail.com>
2018-01-24 19:35:10 -0600

    0.0.5
    Getting close to finishing the motorcycle :+1:

--- handling_mod/handling.lua
+++ handling_mod/handling.lua
@@ -29,7 +29,7 @@ end
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-		DrawMissionText2("Handling Version: ~g~0.0.4~s~")
+		DrawMissionText2("Handling Version: ~g~0.0.5~s~")
 	end
 end)
 
